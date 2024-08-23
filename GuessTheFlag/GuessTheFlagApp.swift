c4b19faf4d7cf5597d32c441d98cbe4ecb3bfd28
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-23 13:03:30 -0600

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Juan Carlos Robledo Morales on 23/08/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
