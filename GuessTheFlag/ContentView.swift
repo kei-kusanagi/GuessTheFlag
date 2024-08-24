2971cc83d6730cf41a0daa7a6194da7fd8fbf09c
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-23 19:12:22 -0600

    Project 2, part 1

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,63 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
-        VStack {
-            //            VStack(spacing: 0){
-            //                Color.red
-            //                Color.blue
-            //            }
-            //            RadialGradient(colors: [.red, .black], center: .center, startRadius: 20, endRadius: 100)
-            //            LinearGradient(colors: [.red, .purple], startPoint: .top, endPoint: .bottom)
-            //            AsyncImage(url: URL(string: "https://images.seeklogo.com/logo-png/52/1/macross-logo-png_seeklogo-527293.png")) { image in
-            //                image
-            //                    .resizable()
-            //                    .aspectRatio(contentMode: .fit)
-            //                    .frame(width: 200, height: 200)
-            //            } placeholder: {
-            //                ProgressView()
-            //            }
-            //            AngularGradient(colors: [.red, .yellow, .green, .blue, .purple, .red], center: .center)
-            //            Button("Delete selection", action: executeDelete)
-            //
-            //
-            //            Button("Button 1"){}
-            //                .buttonStyle(.bordered)
-            //
-            //            Button("Button 2", role: .destructive){}
-            //                .buttonStyle(.bordered)
-            //
-            //            Button("Button 3"){}
-            //                .buttonStyle(.borderedProminent)
-            //                .tint(.indigo)
-            //
-            //            Button("Button 4", role: .destructive){}
-            //                .buttonStyle(.borderedProminent)
-            //
-            //
-            //
-            //        }.edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            ////            .foregroundStyle(.white)
-            //            .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
-            //            .background(.green.gradient)
-//            Image(decorative: "Macross")
-//            
-//            Image(systemName: "pencil.circle.fill")
-//                .foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
-//                .font(.largeTitle)
-            
-            Button{
-                print("Button was tapped")
-            } label: {
-                Label("Edit", systemImage: "pencil")
-                    .padding()
-                    .foregroundColor(.white)
-                    .background(.red)
-            }
+        Button("Show alert"){
+            showingAlert = true
+        }.buttonStyle(.borderedProminent)
+        .alert("Import message", isPresented: $showingAlert){
+            Button("Delete", role: .destructive) { }
+            Button("Cancel", role: .cancel) { }
+        }message: {
+            Text("Please read this")
         }
-    }
-    func executeDelete(){
-        print("Now deleting...")
+        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+        .background(.purple.gradient)
     }
 }
 
