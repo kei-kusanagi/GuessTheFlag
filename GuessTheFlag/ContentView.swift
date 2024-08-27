a77aea0c1ac9c790a4dc11752b2cbc96dd94faf7
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-26 18:00:20 -0600

    Day 21 – Project 2, part two

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,23 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var showingAlert = false
-    
-    var body: some View {
-        Button("Show alert"){
-            showingAlert = true
-        }.buttonStyle(.borderedProminent)
-        .alert("Import message", isPresented: $showingAlert){
-            Button("Delete", role: .destructive) { }
-            Button("Cancel", role: .cancel) { }
-        }message: {
-            Text("Please read this")
+    @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
+    @State var correctAnswer = Int.random(in: 0...2)
+@State private var showingScore = false
+    @State private var scoreTitle = ""
+
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            scoreTitle = "Correcto"
+        } else {
+            scoreTitle = "Incorrecto"
         }
-        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
-        .background(.purple.gradient)
+
+        showingScore = true
+    }
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    var body: some View {
+        
+        ZStack {
+            RadialGradient(stops: [
+                .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
+            ], center: .top, startRadius: 200, endRadius: 400)
+                .ignoresSafeArea()
+
+
+            VStack {
+                Spacer()
+                Text("Guess the Flag")
+                    .font(.largeTitle.weight(.bold))
+                    .foregroundStyle(.white)
+                VStack(spacing: 15){
+                    VStack{
+                        Text("Tap the flag of")
+                            .font(.subheadline.weight(.heavy))
+
+                            .foregroundStyle(.secondary)
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
+                    }
+                    ForEach(0..<3){number in
+                        Button{
+                            flagTapped(number)
+                        } label: {
+                            Image(countries[number])
+                                .clipShape(.capsule)
+                                    .shadow(radius: 5)
+                        }
+                    }
+                }.frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .padding(.vertical, 20)
+                    .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+            }.padding()
+            
+        }
+        .alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continuar", action: askQuestion)
+        } message: {
+            Text("Tu puntuación es ???")
+        }
+//        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
+//        .background(.blue.gradient)
+//        .ignoresSafeArea()
     }
 }
-
 #Preview {
     ContentView()
 }
