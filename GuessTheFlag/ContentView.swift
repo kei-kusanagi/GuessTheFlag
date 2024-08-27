40bcaee281a557e8259349e6c6a10be20c84e78b
Juan Carlos Robledo Morales <kei_kusanagi@outlook.com>
2024-08-27 13:43:08 -0600

    Day 22 – Project 2, part three (chalenge)

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -11,32 +11,47 @@ struct ContentView: View {
     @State var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"].shuffled()
     @State var correctAnswer = Int.random(in: 0...2)
 @State private var showingScore = false
+@State private var showingFinalScore = false
+    
     @State private var scoreTitle = ""
+    @State private var userScore: Int = 0
+    @State private var globalCount: Int = 8
 
     func flagTapped(_ number: Int) {
         if number == correctAnswer {
-            scoreTitle = "Correcto"
+            scoreTitle = "✅ Correct ✅"
+            userScore += 1
+            
         } else {
-            scoreTitle = "Incorrecto"
+            scoreTitle = "❌ Incorrect 😅"
         }
-
-        showingScore = true
+        globalCount -= 1
+        if globalCount == 0 {
+            showingFinalScore = true
+        } else {
+            showingScore =  true
+        }
     }
     func askQuestion() {
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
+    
+    func resetGame(){
+        userScore = 0
+        globalCount = 8
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
     var body: some View {
-        
         ZStack {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
             ], center: .top, startRadius: 200, endRadius: 400)
                 .ignoresSafeArea()
-
-
-            VStack {
+           VStack {
                 Spacer()
                 Text("Guess the Flag")
                     .font(.largeTitle.weight(.bold))
@@ -53,6 +68,7 @@ struct ContentView: View {
                     ForEach(0..<3){number in
                         Button{
                             flagTapped(number)
+                            
                         } label: {
                             Image(countries[number])
                                 .clipShape(.capsule)
@@ -67,9 +83,14 @@ struct ContentView: View {
                 Spacer()
                 Spacer()
                 
-                Text("Score: ???")
-                    .foregroundStyle(.white)
+               VStack {
+                   Text("Score: \(userScore)")
+                        .foregroundStyle(.white)
                     .font(.title.bold())
+                   Text("Remaining tries: \(globalCount)")
+                        .foregroundStyle(.white)
+                    .font(.title.bold())
+               }
                 Spacer()
             }.padding()
             
@@ -77,11 +98,13 @@ struct ContentView: View {
         .alert(scoreTitle, isPresented: $showingScore) {
             Button("Continuar", action: askQuestion)
         } message: {
-            Text("Tu puntuación es ???")
+            Text("Your score is: \(userScore)")
         }
-//        .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, maxHeight: .infinity)
-//        .background(.blue.gradient)
-//        .ignoresSafeArea()
+        .alert("Game Over", isPresented: $showingFinalScore) {
+                    Button("Restart Game 🔁", action: resetGame)
+                } message: {
+                    Text("🎊 Final Score: \(userScore) 🎊")
+                }
     }
 }
 #Preview {
